994180515e47f3f8785e3677d32b1b6f3ad3de81
encarnacao <cencarnacaoq@gmail.com>
2022-11-30 16:53:47 -0300

    Reset e Global

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import './App.css';
+import styled from 'styled-components';
+import { Reset, GlobalStyle } from './Styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello World
-      </h1>
+    <div>
+      <Reset />
+      <GlobalStyle />
+      <Aplicativo>
+        <h1>
+          Hello World
+        </h1>
+      </Aplicativo>
     </div>
   );
 }
+
+const Aplicativo = styled.div`
+  text-align: center;
+  background: #323844;
+  height: 100vh;
+  h1{
+    color: #fff;
+    font-size: 50px;
+    font-weight: 700;
+  }
+`;
 
 export default App;
