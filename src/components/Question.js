56f4164b64549389bd5e92d935f62619effe8f6d
encarnacao <cencarnacaoq@gmail.com>
2022-12-05 15:07:47 -0300

    Teste para correção automatica

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,13 +4,12 @@ import playImage from '../img/seta_play.svg';
 export default function Question(props) {
     const colors = {zap: "#2fbe34", forgotten: "#ff3030", partial: "#FF922E"};
     const playable = props.status === "none";
+    const flashcardText = `Pergunta ${props.num}`;
     const textColor = colors[props.status];
     const playButton = <img data-test="play-btn" onClick={() => props.setClicked(!props.clicked)} src={playImage} alt="play" />;
     return (
         <QuestionDiv textColor={textColor} status={playable ? 0 : 1} clicked={props.clicked ? 1 : 0}>
-            <div>
-                <h1 data-test={!props.clicked?"flashcard-text":"none"}>Pergunta {props.num}</h1>
-            </div>
+            <h1 data-test={!props.clicked?"flashcard-text":"none"}>{flashcardText}</h1>
             {playable?playButton:props.result}
         </QuestionDiv>
     );
@@ -23,7 +22,7 @@ const QuestionDiv = styled.div`
     align-items: center;
     padding: 0 15px;
     display: ${props => (props.clicked ? 'none' : 'flex')};
-    > div {
+    > h1 {
         font-size: 16px;
         font-family: 'Recursive', sans-serif;
         font-weight: 700;
