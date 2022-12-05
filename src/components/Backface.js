540b76658991485e04d414987ab2b26bb03fc168
encarnacao <cencarnacaoq@gmail.com>
2022-12-05 15:21:30 -0300

    Refatoração

--- src/components/Backface.js
+++ src/components/Backface.js
@@ -6,41 +6,38 @@ import forgottenImage from '../img/icone_erro.svg';
 
 
 
-export default function Frontface(props){
-    function zap(){
+export default function Frontface(props) {
+    function zap() {
         const result = <img data-test="zap-icon" src={zapImage} alt="zap" />;
-        props.setAnswers([...props.answers,result]);
-        props.setResult(result);
-        props.setStatus("zap");
-        reset();
+        props.setAnswers([...props.answers, result]);
+        reset(result,"zap");
     }
 
-    function forgotten(){
+    function forgotten() {
         const result = <img data-test="no-icon" src={forgottenImage} alt="no" />;
         props.setError(true);
-        props.setAnswers([...props.answers,result]);
-        props.setResult(result);
-        props.setStatus("forgotten");
-        reset();
+        props.setAnswers([...props.answers, result]);
+        reset(result,"forgotten");
     }
 
-    function  partial(){
+    function partial() {
         const result = <img data-test="partial-icon" src={partialImage} alt="partial" />;
-        props.setAnswers([...props.answers,result]);
-        props.setResult(result);
-        props.setStatus("partial");
-        reset();
+        props.setAnswers([...props.answers, result]);
+        reset(result,"partial");
     }
 
-    function reset(){
-        props.setAnswered(props.answered+1);
+    function reset(result, status) {
+        props.setAnswered(props.answered + 1);
+        props.setResult(result);
+        props.setStatus(status);
         props.setTurn(false);
         props.setClicked(false);
     }
+    
     const dataTestRender = props.clicked && props.turn;
     return (
-        <BackDiv clicked={props.clicked?1:0}>
-            <TextDiv data-test={dataTestRender?"flashcard-text":"none"}>{props.answer}</TextDiv>
+        <BackDiv clicked={props.clicked ? 1 : 0}>
+            <h1 data-test={dataTestRender ? "flashcard-text" : "none"}>{props.answer}</h1>
             <ButtonsDiv>
                 <Button data-test="no-btn" onClick={forgotten} color="#ff3030">Não lembrei</Button>
                 <Button data-test="partial-btn" onClick={partial} color="#FF922E">Quase não lembrei</Button>
@@ -53,26 +50,25 @@ export default function Frontface(props){
 const BackDiv = styled.div`
     width: 100%;
     height: 100%;
-    display: ${props => (props.clicked?'flex':'none')};
+    display: ${props => (props.clicked ? 'flex' : 'none')};
     flex-direction: column;
     justify-content: space-evenly;
     margin: 0;
     padding: 15px;
+    > h1 {
+        width: 100%;
+        font-family: 'Recursive', sans-serif;
+        font-size: 18px;
+        margin: 0 auto 10px auto;
+    }
 `;
 
-const TextDiv = styled.div`
-    width: 100%;
-    font-family: 'Recursive', sans-serif;
-    font-size: 18px;
-    margin: 0 auto 10px auto;
-`;
 
 const ButtonsDiv = styled.div`
     display: flex;
     width: 100%;
     justify-content: space-between;
     margin: 0 auto;
-
 `;
 
 const Button = styled.button`
--- src/components/Frontface.js
+++ src/components/Frontface.js
@@ -5,7 +5,7 @@ export default function Frontface(props){
     const dataTestRender = props.clicked && !props.turn;
     return (
         <FrontDiv clicked={props.clicked?1:0}>
-            <p data-test={dataTestRender?"flashcard-text":"none"}>{props.question}</p>
+            <h1 data-test={dataTestRender?"flashcard-text":"none"}>{props.question}</h1>
             <Turn data-test="turn-btn" onClick={()=>props.setTurn(!props.turn)} src={turn} alt="Virar" />
         </FrontDiv>
     );
@@ -15,7 +15,7 @@ const FrontDiv = styled.div`
     width: 100%;
     height: 100%;
     display: ${props => (props.clicked?'flex':'none')};
-    > p{
+    > h1{
         font-family: 'Recursive', sans-serif;
         font-size: 18px;
         margin: 15px;
